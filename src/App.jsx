e7bee79b4210b5cd8fe52143409a21d339b24ca5
Ju4mpizel <juampizel@gmail.com>
2026-04-07 15:18:14 -0400

    Pokedex Concluida

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,42 @@ import viteLogo from "./assets/vite.svg";
 import PokemonCard from "./components/PokemonCard";
 
 function App() {
-  const [pokemonData, setPokemonData] = useState(null);
+  const [pokemonData, setPokemonData] = useState([]);
   const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
 
   const fetchPokemons = async (id) => {
     try {
       const response = await fetch(`${BASE_URL}${id}`);
       const data = await response.json();
-      console.log(data);
-      setPokemonData(data);
+
+      setPokemonData((prevpokemonData) => [...prevpokemonData, data]);
     } catch (error) {
       console.error("ERROR CON EL FETCH DE POKEMON");
     }
   };
 
+  const fetchAllPokemons = () => {
+    setPokemonData([]);
+    for (let i = 1; i < 19; i++) {
+      fetchPokemons(i);
+    }
+  };
+
   useEffect(() => {
-    fetchPokemons(1);
+    fetchAllPokemons();
   }, []);
+
+  useEffect(() => {
+    console.log(pokemonData);
+  }, [pokemonData]);
   return (
     <>
       <h1 className="text-4xl font-semibold text-center my-6">POKE API DEMO</h1>
       <div className="px-4">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          <PokemonCard pokemonData={pokemonData} />
+          {pokemonData.map((pokemon, index) => (
+            <PokemonCard key={index} pokemonData={pokemon}></PokemonCard>
+          ))}
         </div>
       </div>
     </>
